452deeb6b90d8d853eea3f01fd7c529d1ffe05d6
juliettenicol <105407539+juliettenicol@users.noreply.github.com>
2022-05-30 15:00:36 +1000

    update slidermap

--- script.js
+++ script.js
@@ -56,25 +56,19 @@ function initComparisons() {
 
 initComparisons();
 
+//tabs 
 
-
-
-/*tabs*/
-
-const tabs = document.querySelectorAll('[data-tab-target]')
-const tabContents = document.querySelectorAll('[data-tab-content]')
-
-tabs.forEach(tab => {
+document.querySelectorAll('[data-tab-target]').forEach(tab => {
   tab.addEventListener('click', () => {
     const target = document.querySelector(tab.dataset.tabTarget)
-    tabContents.forEach(tabContent => {
+    document.querySelectorAll('[data-tab-content]').forEach(tabContent => {
       tabContent.classList.remove('active')
     })
-    tabs.forEach(tab => {
+    document.querySelectorAll('[data-tab-target]').forEach(tab => {
       tab.classList.remove('active')
     })
     tab.classList.add('active')
     target.classList.add('active')
   })
-})
+});
 
