1e695eb3d7d20ea898eb847152e4e19f248ee179
s3661011 <50191508+s3661011@users.noreply.github.com>
2022-05-31 07:25:59 +1000

    commit

---
+++ mouseover.js
@@ -0,0 +1,98 @@
+function bannockburn() {
+    x = document.getElementById("Bannockburn");
+    x.style.backgroundColor = "#CFEAFD";
+}
+
+function batesford() {
+    x = document.getElementById("batesford");
+    x.style.backgroundColor = "#CFEAFD";
+}
+
+function capeclear() {
+    x = document.getElementById("capeclear");
+    x.style.backgroundColor = "#CFEAFD";
+}
+
+function dereel() {
+    x = document.getElementById("dereel");
+    x.style.backgroundColor = "#CFEAFD";
+}
+
+function haddon() {
+    x = document.getElementById("haddon");
+    x.style.backgroundColor = "#CFEAFD";
+}
+
+function inverleigh() {
+    x = document.getElementById("inverleigh");
+    x.style.backgroundColor = "#CFEAFD";
+}
+
+function lethbridge() {
+    x = document.getElementById("lethbridge");
+    x.style.backgroundColor = "#CFEAFD";
+}
+
+function linton() {
+    x = document.getElementById("linton");
+    x.style.backgroundColor = "#CFEAFD";
+}
+
+function maude() {
+    x = document.getElementById("maude");
+    x.style.backgroundColor = "#CFEAFD";
+}
+
+function meredith() {
+    x = document.getElementById("meredith");
+    x.style.backgroundColor = "#CFEAFD";
+}
+
+function napoleons() {
+    x = document.getElementById("napoleons");
+    x.style.backgroundColor = "#CFEAFD";
+}
+
+function rokewood() {
+    x = document.getElementById("rokewood");
+    x.style.backgroundColor = "#CFEAFD";
+}
+
+function scarsdale() {
+    x = document.getElementById("scarsdale");
+    x.style.backgroundColor = "#CFEAFD";
+}
+
+function shelford() {
+    x = document.getElementById("shelford");
+    x.style.backgroundColor = "#CFEAFD";
+}
+
+function smythesdale() {
+    x = document.getElementById("smythesdale");
+    x.style.backgroundColor = "#CFEAFD";
+}
+
+function teesdale() {
+    x = document.getElementById("teesdale");
+    x.style.backgroundColor = "#CFEAFD";
+}
+
+function normal() {
+    document.getElementById("Bannockburn").style.backgroundColor = "#f5dc9f";
+    document.getElementById("batesford").style.backgroundColor = "#f5dc9f";
+    document.getElementById("capeclear").style.backgroundColor = "#f5dc9f";
+    document.getElementById("dereel").style.backgroundColor = "#f5dc9f";
+    document.getElementById("haddon").style.backgroundColor = "#f5dc9f";
+    document.getElementById("inverleigh").style.backgroundColor = "#f5dc9f";
+    document.getElementById("lethbridge").style.backgroundColor = "#f5dc9f";
+    document.getElementById("linton").style.backgroundColor = "#f5dc9f";
+    document.getElementById("maude").style.backgroundColor = "#f5dc9f";
+    document.getElementById("meredith").style.backgroundColor = "#f5dc9f";
+    document.getElementById("napoleons").style.backgroundColor = "#f5dc9f";
+    document.getElementById("rokewood").style.backgroundColor = "#f5dc9f";
+    document.getElementById("scarsdale").style.backgroundColor = "#f5dc9f";
+    document.getElementById("shelford").style.backgroundColor = "#f5dc9f";
+    document.getElementById("smythesdale").style.backgroundColor = "#f5dc9f";
+    document.getElementById("teesdale").style.backgroundColor = "#f5dc9f";
+}
